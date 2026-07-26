163ac51b772210ced4aec9035f08741d88fbb0cd
trisharaj06 <trirox2030@gmail.com>
2026-07-26 15:55:59 +0530

    first commit

---
+++ app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('./app');
+
+describe('GET /', () => {
+  it('should return 200 OK and greeting message', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe('Hello, CI/CD World!');
+  });
+});
